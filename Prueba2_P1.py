e380b15989baf5f7b2d8bd43d3bfba867ac5ce27
vicenteestebanhuenulef <vicenteesteban.huenulef@alumnos.ulagos.cl>
2024-06-27 15:20:07 -0400

    Add files via upload

---
+++ Prueba2_P1.py
@@ -0,0 +1,13 @@
+
+lista = []
+promedio=0
+suma=0
+
+while valor != "0":
+    lista.append(valor)
+    valor = input("Ingresar notas(ingresar 0 para finalizar : ")
+   
+
+print(valor)
+
+    
---
+++ Prueba2_P2.py
@@ -0,0 +1,12 @@
+lista =[]
+
+for x in range(10):
+        productos= print(input("Ingrese precio productos a dolares:"))
+        lista.append(productos)
+
+
+print(lista)
+
+
+
+
